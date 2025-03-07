4ca069a7dcd38e303a3f8773a43646c5d0daddc1
gumends <gustavo08.mendes@gmail.com>
2025-03-07 00:04:27 -0300

    Retirada propriedade de idade e alterado para cpf

--- src/main/java/com/gustavo/auth/dto/RegisterDto.java
+++ src/main/java/com/gustavo/auth/dto/RegisterDto.java
@@ -3,11 +3,10 @@ package com.gustavo.auth.dto;
 import com.gustavo.auth.model.UserRole;
 
 public record RegisterDto(
-        String login,
         String senha,
         UserRole role,
         String nome,
-        int idade,
+        String cpf,
         String email
 ) {
 }
