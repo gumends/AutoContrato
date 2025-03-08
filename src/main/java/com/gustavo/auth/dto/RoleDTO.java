2748d4f7a3243e818ca3e1ec0d1b39e169ce8aa5
gumends <gustavo08.mendes@gmail.com>
2025-03-08 02:17:08 -0300

    Novo DTO do role

---
+++ src/main/java/com/gustavo/auth/dto/RoleDTO.java
@@ -0,0 +1,14 @@
+package com.gustavo.auth.dto;
+
+public class RoleDTO {
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
+
